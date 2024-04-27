7279bd76fbb262a59a772a2575a16ca536fa28de
yashrajbharticybtekk <y.bharti@cybtekk.com>
2024-04-28 01:26:11 +0530

    add photo capture js

---
+++ js/photo-capture-and-save.mjs
@@ -0,0 +1,10 @@
+export const capturePhoto = () => {
+  const photoButton = document.querySelector(".capture-button");
+  photoButton.addEventListener("click", () => {
+    const isVideoMode = document.querySelector(
+      ".switch-camera-video-photo-mode input[type='checkbox']"
+    ).checked;
+    if (isVideoMode) return;
+    photoButton.classList.add("click");
+  });
+};
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { streamWebCamVideo } from "./js/stream.mjs";
 import { changeFacingMode } from "./js/camera-facing-mode.mjs";
+import { capturePhoto } from "./js/photo-capture-and-save.mjs";
 
 streamWebCamVideo();
 changeFacingMode();
+capturePhoto();
