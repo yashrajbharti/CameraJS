0fae49675d290e10bf2ef1329f1a6ccb1bd62c60
yashrajbharticybtekk <y.bharti@cybtekk.com>
2024-04-28 00:33:17 +0530

    add toggle camera js

--- js/stream.mjs
+++ js/stream.mjs
@@ -1,12 +1,19 @@
 export const streamWebCamVideo = () => {
   const video = document.getElementById("stream");
+  let isFrontCamera = true; // Variable to track camera direction
+  const constraints = {
+    video: {
+      facingMode: isFrontCamera ? "user" : "environment", // Toggle between front and rear cameras
+    },
+  };
   window.navigator.mediaDevices
-    .getUserMedia({ video: true })
+    .getUserMedia(constraints)
     .then((stream) => {
       video.srcObject = stream;
       video.onloadedmetadata = (e) => {
         video.play();
       };
+      isFrontCamera = !isFrontCamera;
     })
     .catch((e) => {
       console.error(e);
