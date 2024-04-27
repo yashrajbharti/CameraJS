b0dbe02f8ae8e8e9637c83f2f2e00393732dd568
yashrajbharticybtekk <y.bharti@cybtekk.com>
2024-04-28 04:51:09 +0530

    add photo capture scale js

--- js/photo-capture-and-save.mjs
+++ js/photo-capture-and-save.mjs
@@ -20,7 +20,7 @@ const drawOnCanvasAndSavePhoto = async (isMirrored = false) => {
   const video = document.getElementById("stream");
   const canvas = document.createElement("canvas");
   const context = canvas.getContext("2d");
-  const scaleFactor = 5;
+  const scaleFactor = 2;
   canvas.width = video.videoWidth * scaleFactor;
   canvas.height = video.videoHeight * scaleFactor;
   if (isMirrored) {
